96b23393fd3402a49765adcfda8be854a60e2e86
jonathan.essex <jonathan.essex@misys.com>
2016-12-20 13:47:15 -0500

    Initial commit of some Drupes

---
+++ JavaDrupe/src/org/javadrupe/collections/UnmodifiableList.java
@@ -0,0 +1,145 @@
+package org.javadrupe.collections;
+
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/** The ubiquitous unmodifiable list class.
+ * 
+ * A not-bad implementation of the ubiquitous unmodifiable list class. Why the fuck isn't this in
+ * java already?
+ * 
+ * TODO: maybe provide better spliterator implementation
+ * 
+ * @author localadmin
+ *
+ * @param <E> Element type.
+ */
+public interface UnmodifiableList<E> extends Iterable<E> {
+	
+	@SuppressWarnings("rawtypes")
+	public static final UnmodifiableList EMPTY = new Empty();
+
+	/** Get first element in list. */
+	public E head();
+	/** Get sublist containing all elements in list after first. */
+	public UnmodifiableList<E> tail();
+	/** Create a new list, containing all elements in this list plus elem. */
+	public UnmodifiableList<E> cons(E elem);
+	/** Create a new list, containing all elements in this list plus <code>list</code> */
+	public UnmodifiableList<E> join(UnmodifiableList<E> list);
+	/** is this list empty? */
+	public boolean isEmpty();
+	/** get stream */
+	public default Stream<E> stream() { return StreamSupport.stream(spliterator(), false); }
+
+	
+	/** Create an unmodifiable view of an existing list */
+	public static <E> UnmodifiableList<E> view(List<E> to_view) {
+		return new Proxy<E>(to_view);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <E> UnmodifiableList<E> empty() {
+		return (UnmodifiableList<E>)EMPTY;
+	}
+	
+	@SafeVarargs
+	public static <E> UnmodifiableList<E> as(E... es) {
+		return new Proxy<E>(Arrays.asList(es));
+	}
+	
+	public static class EmptyIterator<E> implements Iterator<E> {
+		public boolean hasNext() { return false; }
+		public E next() { return null; }
+	}
+
+	/** Why can't I make this fucking private, java?? */
+	public static class Empty<E> implements UnmodifiableList<E> {
+		@Override
+		public E head() { return null; }
+		@Override
+		public UnmodifiableList<E> tail() { return null; }
+		@Override
+		public UnmodifiableList<E> cons(E elem) { return new Impl<E>(elem, this); }
+		@Override
+		public boolean isEmpty() { return true; }
+		@Override
+		public UnmodifiableList<E> join(UnmodifiableList<E> list) { return list; }
+		@Override		
+		public Iterator<E> iterator() { return new EmptyIterator<E>(); }
+	}
+	
+	/** Why can't I make this fucking private, java?? */
+	public static class IteratorImpl<E> implements Iterator<E> {
+		private UnmodifiableList<E> next;
+		@Override
+		public boolean hasNext() { return !next.isEmpty(); }
+		@Override
+		public E next() { E rv = next.head(); next = next.tail(); return rv; }
+		public IteratorImpl(UnmodifiableList<E> next) { this.next = next; }
+	}
+	
+	/** Why can't I make this fucking private, java?? */
+	public static class Impl<E> implements UnmodifiableList<E> {
+		private final E head;
+		private final UnmodifiableList<E> tail;
+		public Impl(E head, UnmodifiableList<E> tail) { this.head = head; this.tail = tail; }
+		@Override
+		public boolean isEmpty() { return false; }
+		@Override
+		public E head() { return head; }
+		@Override
+		public UnmodifiableList<E> tail() { return tail; }
+		@Override
+		public UnmodifiableList<E> cons(E elem) { return new Impl<E>(elem, this); }
+		@Override
+		public Iterator<E> iterator() { return new IteratorImpl<E>(this); }
+		@Override
+		public UnmodifiableList<E> join(UnmodifiableList<E> list) { return list.isEmpty() ? this : new Merged<E>(this, list); }
+	}
+	
+	/** Why can't I make this fucking private, java?? */
+	public static class Merged<E> implements UnmodifiableList<E> {
+		UnmodifiableList<E> a;
+		UnmodifiableList<E> b;	
+		public Merged(UnmodifiableList<E> a, UnmodifiableList<E> b) { this.a = a; this.b = b; }
+		@Override
+		public E head() { return a.head(); }
+		@Override
+		public UnmodifiableList<E> tail() { return (a.tail().isEmpty()) ? b : new Merged<E>(a.tail(), b); }
+		@Override
+		public UnmodifiableList<E> cons(E elem) { return new Impl<E>(elem, this); }
+		@Override
+		public Iterator<E> iterator() { return new IteratorImpl<E>(this); }
+		@Override
+		public UnmodifiableList<E> join(UnmodifiableList<E> list) { return list.isEmpty() ? this : new Merged<E>(this, list); }
+		@Override
+		public boolean isEmpty() { return false; }		
+	}	
+
+	/** Why can't I make this fucking private, java?? */
+	public static class Proxy<E> implements UnmodifiableList<E> {
+		
+		private List<E> proxied;
+		
+		public Proxy(List<E> proxied) { this.proxied = proxied; }
+		@Override
+		public boolean isEmpty() { return proxied.isEmpty(); }
+		@Override
+		public Iterator<E> iterator() { return proxied.iterator(); }
+		@Override
+		public E head() { return proxied.get(0); }
+		@Override
+		public UnmodifiableList<E> tail() { return new Proxy<E>(proxied.subList(1, proxied.size())); }
+		@Override
+		public UnmodifiableList<E> cons(E elem) { return new Impl<E>(elem, this); }
+		@Override
+		public UnmodifiableList<E> join(UnmodifiableList<E> list) { return list.isEmpty() ? this : new Merged<E>(this, list);	}
+	}
+
+}
---
+++ JavaDrupe/src/org/javadrupe/xml/TransformerWriter.java
@@ -0,0 +1,344 @@
+package org.javadrupe.xml;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+class TransformerWriter implements XMLStreamWriter {
+	
+	private static class Context {
+		public final QName name;
+		public final TransformerWriter.NamespaceContextImpl namespace;
+		public Context(QName name, TransformerWriter.NamespaceContextImpl namespace) { this.name = name; this.namespace = namespace; }
+	}
+	
+	private TransformerHandler handler;
+	private Stack<TransformerWriter.Context> context = new Stack<TransformerWriter.Context>();
+	private AttributesImpl attributes = new AttributesImpl();
+	private boolean isFinished = false;
+	
+	private static class NamespaceContextImpl implements NamespaceContext {
+		
+		public final TransformerWriter.NamespaceContextImpl parent_context;
+		// map from URI to prefix
+		public final TreeMap<String,String> mapped_prefixes = new TreeMap<String,String>();
+
+		@Override
+		public String getNamespaceURI(String prefix) {
+			Optional<Map.Entry<String, String>> entry = mapped_prefixes.entrySet().stream().filter(v -> v.getValue().equals(prefix)).findFirst();
+			if (entry.isPresent()) return entry.get().getKey();
+			if (parent_context==null) return null;
+			return parent_context.getPrefix(prefix);
+		}
+
+		@Override
+		public String getPrefix(String namespaceURI) {
+			String uri = mapped_prefixes.get(namespaceURI);
+			if (uri != null) return uri;
+			if (parent_context == null) return null;
+			return parent_context.getPrefix(namespaceURI);
+		}
+		
+		public Stream<String> getPrefixesStream(String namespaceURI) {
+			String prefix = mapped_prefixes.get(namespaceURI);
+			if (prefix == null && parent_context == null) return Stream.empty();
+			if (prefix == null) return parent_context.getPrefixesStream(namespaceURI);
+			if (parent_context == null) return Stream.of(prefix);
+			return Stream.concat(Stream.of(prefix), parent_context.getPrefixesStream(namespaceURI));				
+		}
+		
+		@Override
+		public Iterator<String> getPrefixes(String namespaceURI) {
+			return getPrefixesStream(namespaceURI).iterator();
+		}
+		
+		public NamespaceContextImpl(TransformerWriter.NamespaceContextImpl parent) {
+			this.parent_context = parent;
+		}
+		
+		public Collection<String> getLocalURIs() {
+			return mapped_prefixes.keySet();
+		}
+
+		public Collection<String> getLocalPrefixes() {
+			return mapped_prefixes.values();
+		}
+		
+		public void addLocalBinding(String prefix, String URI) {
+			mapped_prefixes.put(URI, prefix);
+		}
+	}
+	
+	private void finishStartingElement() throws XMLStreamException {
+		if (!isFinished) {
+			TransformerWriter.Context current = context.peek();
+			try {
+				for (String uri : current.namespace.getLocalURIs())
+					handler.startPrefixMapping(current.namespace.getPrefix(uri), uri);
+				handler.startElement(current.name.getNamespaceURI(), current.name.getLocalPart(), current.name.getPrefix() + ":" + current.name.getLocalPart(), attributes);
+			} catch (SAXException e) {
+				throw new XMLStreamException(e);
+			}
+			attributes.clear();
+		}			
+	}
+	
+	public void writeStartElement(QName name) throws XMLStreamException {
+		finishStartingElement();
+		context.push(new Context(name, new NamespaceContextImpl(context.peek().namespace)));
+		isFinished = false;	
+	}
+
+	@Override
+	public void writeStartElement(String localName) throws XMLStreamException {
+		writeStartElement(new QName(localName));	
+	}
+
+	@Override
+	public void writeStartElement(String namespaceURI, String localName) throws XMLStreamException {
+		writeStartElement(new QName(namespaceURI, localName));	
+	}
+
+	@Override
+	public void writeStartElement(String prefix, String localName, String namespaceURI) throws XMLStreamException {
+		writeStartElement(new QName(namespaceURI, localName, prefix));
+	}
+
+	public void writeEmptyElement(QName name) throws XMLStreamException {
+		finishStartingElement();
+		try {
+			String qname = name.getPrefix() + ":" + name.getLocalPart();
+			handler.startElement(name.getNamespaceURI(), name.getLocalPart(), qname, attributes);
+			handler.endElement(name.getNamespaceURI(), name.getLocalPart(), qname);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+		attributes.clear();
+	}
+	
+	@Override
+	public void writeEmptyElement(String namespaceURI, String localName) throws XMLStreamException {
+		writeEmptyElement(new QName(namespaceURI, localName));
+	}
+
+	@Override
+	public void writeEmptyElement(String prefix, String localName, String namespaceURI) throws XMLStreamException {
+		writeEmptyElement(new QName(namespaceURI, localName, prefix));
+	}
+
+	@Override
+	public void writeEmptyElement(String localName) throws XMLStreamException {
+		writeEmptyElement(new QName(localName));
+	}
+
+	@Override
+	public void writeEndElement() throws XMLStreamException {
+		finishStartingElement();
+		try {
+			TransformerWriter.Context current = context.pop();
+			String qname = current.name.getPrefix() + ":" + current.name.getLocalPart();
+			handler.endElement(current.name.getNamespaceURI(), current.name.getLocalPart(), qname);
+			for (String prefix : current.namespace.getLocalPrefixes())
+				handler.endPrefixMapping(prefix);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+		attributes.clear();		
+	}
+
+	@Override
+	public void writeEndDocument() throws XMLStreamException {
+		try {
+			handler.endDocument();
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+	}
+
+	@Override
+	public void close() throws XMLStreamException {
+	}
+
+	@Override
+	public void flush() throws XMLStreamException {
+	}
+
+	@Override
+	public void writeAttribute(String localName, String value) throws XMLStreamException {
+		writeAttribute("",localName,value);
+	}
+
+	@Override
+	public void writeAttribute(String prefix, String namespaceURI, String localName, String value)
+			throws XMLStreamException {
+		String qname = prefix + ":" + localName;
+		attributes.addAttribute(namespaceURI, localName, qname, "", value );
+		
+	}
+
+	@Override
+	public void writeAttribute(String namespaceURI, String localName, String value) throws XMLStreamException {
+		writeAttribute("",namespaceURI, localName,value);			
+	}
+
+	@Override
+	public void writeNamespace(String prefix, String namespaceURI) throws XMLStreamException {
+		try {
+			context.peek().namespace.addLocalBinding(prefix, namespaceURI);;
+			handler.startPrefixMapping(prefix, namespaceURI);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+		
+	}
+
+	@Override
+	public void writeDefaultNamespace(String namespaceURI) throws XMLStreamException {
+		writeNamespace("", namespaceURI);	
+	}
+
+	@Override
+	public void writeComment(String data) throws XMLStreamException {
+		finishStartingElement();	
+		try {
+			handler.comment(data.toCharArray(), 0, data.length());
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+	}
+
+	@Override
+	public void writeProcessingInstruction(String target) throws XMLStreamException {
+		writeProcessingInstruction(target,"");
+	}
+
+	@Override
+	public void writeProcessingInstruction(String target, String data) throws XMLStreamException {
+		finishStartingElement();
+		try {
+			handler.processingInstruction(target, data);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}			
+	}
+
+	@Override
+	public void writeCData(String data) throws XMLStreamException {
+		finishStartingElement();
+		try {
+			handler.startCDATA();
+			handler.characters(data.toCharArray(), 0, data.length());
+			handler.endCDATA();
+			
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}			
+	}
+
+	@Override
+	public void writeDTD(String dtd) throws XMLStreamException {
+		finishStartingElement();
+		String[] elements = dtd.split("\\s+");
+		String name = elements.length > 0 ? elements[0] : "";
+		String publicId = elements.length > 1 ? elements[1] : "";
+		String systemId = elements.length > 2 ? elements[2] : "";
+		
+		try {
+			handler.startDTD(name, publicId, systemId);
+			handler.endDTD();
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}		
+	}
+
+	@Override
+	public void writeEntityRef(String name) throws XMLStreamException {
+		finishStartingElement();
+		try {
+			handler.startEntity(name);
+			handler.endEntity(name);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}		
+	}
+
+	@Override
+	public void writeStartDocument() throws XMLStreamException {
+		try {
+			handler.startDocument();
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}		
+	}
+
+	@Override
+	public void writeStartDocument(String version) throws XMLStreamException {
+		writeStartDocument();
+	}
+
+	@Override
+	public void writeStartDocument(String encoding, String version) throws XMLStreamException {
+		writeStartDocument();			
+	}
+
+	@Override
+	public void writeCharacters(String text) throws XMLStreamException {
+		writeCharacters(text.toCharArray(), 0, text.length());
+		
+	}
+
+	@Override
+	public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
+		finishStartingElement();
+		try {
+			handler.characters(text, start, len);
+		} catch (SAXException e) {
+			throw new XMLStreamException(e);
+		}
+	}
+
+	@Override
+	public String getPrefix(String uri) throws XMLStreamException {
+		return context.peek().namespace.getPrefix(uri);
+	}
+
+	@Override
+	public void setPrefix(String prefix, String uri) throws XMLStreamException {
+		context.peek().namespace.addLocalBinding(prefix, uri);			
+	}
+
+	@Override
+	public void setDefaultNamespace(String uri) throws XMLStreamException {
+		context.peek().namespace.addLocalBinding("", uri);			
+	}
+
+	@Override
+	public void setNamespaceContext(NamespaceContext context) throws XMLStreamException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public NamespaceContext getNamespaceContext() {
+		return context.peek().namespace;
+	}
+
+	@Override
+	public Object getProperty(String name) throws IllegalArgumentException {
+		return handler.getTransformer().getOutputProperty(name);
+	}
+
+	
+}
